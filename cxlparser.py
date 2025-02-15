8c8a53414ea7e0c4a0ec2c956df8f12f49c7bdb5
Isaac Sussman <isaac.r.sussman@gmail.com>
2025-02-14 19:21:48 -0500

    Finished cxlparser, fixed it to support all features (OTHER THAN PHRASE-PHRASE LINKS)
    Changes to be committed:
    	renamed:    1049.cmap -> cmaps/1049.cmap
    	renamed:    AeroCapture.cxl -> cmaps/AeroCapture.cxl
    	new file:   cmaps/Bacterial Characteristics.cxl
    	new file:   cmaps/Splitting example.cxl
    	renamed:    Untitled 1.cmap -> cmaps/Untitled 1.cmap
    	new file:   cmaps/Untitled 1.cxl
    	new file:   cmaps/Untitled 2.cxl
    	new file:   cmaps/Untitled 3.cxl
    	renamed:    concept_map_example.cxl -> cmaps/concept_map_example.cxl
    	renamed:    simple_map.cxl -> cmaps/simple_map.cxl
    	modified:   cxl.ipynb
    	modified:   cxlparser.py
    	modified:   environment.yml
    	modified:   main.py

--- cxlparser.py
+++ cxlparser.py
@@ -1,38 +1,85 @@
 from __future__ import annotations 
 
-import xml.etree
 import xml.etree.ElementTree
 import defusedxml.ElementTree as ET
-import defusedxml.minidom as minidom
 import xml
 import networkx as nx
 import matplotlib.pyplot as plt
+import os
+
+# Systenmatic lit review
+
 class InvalidCmapFileException(Exception):
     def __init__(self, message, file_path):
         super().__init__(message)
         self.file_path = file_path
 
+class Connection:
+    def __init__(self, from_id, to_id, id:str = None, label: str = None):
+        self.label = label
+        self.id = id
+        self.from_id = from_id
+        self.to_id = to_id
+    
+    def __str__(self):
+        return f"Label: {self.label} id:{self.id} {self.from_id}->{self.to_id}"
 
-class cxl:
-    def __init__(self, path: str) -> None:
+
+        
+
+class BundlePhrase:
+    ### doesn't work w/ phrase-phrase connections ###
+    def __init__(self, label: str, id:str, from_list = [], to_list = []):
+        self.from_list = from_list
+        self.to_list = to_list
+        self.label = label
+        self.id = id
+    
+    def add_connection(self, from_id:str = None, to_id: str = None):
+        if from_id:
+            self.from_list.append(from_id)
+        if to_id:
+            self.to_list.append(to_id)
+    
+    def list_connections(self)->list[dict[str:str]]:
+        connections = []
+        for f in self.from_list:
+            for t in self.to_list:
+                connections.append(Connection(f, t, label=self.label))
+        return connections
+    
+    def __str__(self):
+        s = ""
+        for i in self.list_connections():
+            s+=f"{i}\n"
+        return s
+
+
+class Cxl:
+    def __init__(self, path: str, fuzzy:bool = False) -> None:
         self._tree = ET.parse(path)
         self.concepts_by_id: dict = None
         self.concepts_by_label: dict = None
-        self.connections_by_id: dict = None
-        self.connections_by_from_id: dict = None
-        self.connections_by_to_id: dict = None
-        self.graph = nx.DiGraph()
+        self.connections: list[Connection] = None
+        # self.connections_by_from_id: dict = None
+        # self.connections_by_to_id: dict = None
+        self.graph = nx.DiGraph() if not fuzzy else nx.Graph()
         self.phrases_by_id: dict = None
-        self.phrases_by_label: dict = None
+        # self.phrases_by_label: dict = None
         self._file_path = ""
+        self.fuzzy = fuzzy
 
     
-    def import_data(self, path: str) -> None:
-        self._file_path = path
+    def import_data(self, path: str = None) -> None:
+        if path:
+            self._file_path = path
+
+        if (not self._file_path) or self._file_path == "" or not os.path.exists(self._file_path):
+            raise FileNotFoundError(self._file_path)
         self._tree = ET.parse(path)
 
 
-    def _fix_connections(self):
+    """def _fix_connections(self):
         l = []
         for i in self.connections_by_id:
             item = self.connections_by_id[i]
@@ -45,15 +92,20 @@ class cxl:
                 print(i in self.connections_by_from_id.keys(), i, self.connections_by_from_id.keys())
             item = self.connections_by_from_id[i]
             del self.connections_by_id[item["id"]]
-            del self.connections_by_from_id[item["from-id"]]
+            del self.connections_by_from_id[item["from-id"]]"""
             
                 
-            # print("THIS ONE -> "+str(item))
     @staticmethod
-    def show_map(g: cxl) -> None:
-        nx.draw_networkx(g.graph, with_labels=True, labels={i:g.concepts_by_id[i]["label"] for i in g.concepts_by_id})
+    def show_map(map: Cxl) -> None:
+        d = {i:map.concepts_by_id[i]["label"] for i in map.concepts_by_id}
+        e = {i:map.phrases_by_id[i]['label'] for i in map.phrases_by_id}
+        print("nodes: ", [f'{d[i]} {i}' for i in list(map.graph.nodes)], f"\nd:{d}")
+        G = map.graph
+        nx.draw_networkx(map.graph, with_labels=True, labels=d, font_size = 8, pos=nx.shell_layout(G, scale=2))
+        nx.draw_networkx_edge_labels(map.graph,nx.shell_layout(map.graph, scale=2), edge_labels=nx.get_edge_attributes(map.graph,'label'))
         plt.show()
         
+
     def parse_map(self):
         concept_list = []
         for i in self._tree.getroot()[1][0].iter():
@@ -67,17 +119,7 @@ class cxl:
         else:
             self.concepts_by_label = None
 
-        connection_list = []
-        for i in self._tree.getroot()[1][2].iter():
-            if i.attrib == {}:
-                continue
-            connection_list.append(i.attrib)
-        self.connections_by_id = {x["id"]:x for x in connection_list}
-        self.connections_by_from_id = {x["from-id"]:x for x in connection_list}
-        self.connections_by_to_id = {x["to-id"]:x for x in connection_list}
-
-        '''for i in self.concepts_by_id:
-            self.graph.add_node(i)'''
+        # PHRASE LIST
         phrase_list = []
         for i in self._tree.getroot()[1][1].iter():
             if i.attrib == {}:
@@ -85,38 +127,77 @@ class cxl:
             phrase_list.append(i.attrib)
         self.phrases_by_id = {x["id"]:x for x in phrase_list}
         clm = [x["label"] for x in phrase_list]
+            # BY LABEL
         if len(clm) == len(set(clm)):
             self.phrases_by_label = {x["label"]:x for x in phrase_list}
-        self._fix_connections()
-        for i in self.connections_by_id.values():
-            if i["from-id"] in self.phrases_by_id:
+
+        # ALL LISTED CONNECTIONS
+        intital_connection_ids = []
+        for i in self._tree.getroot()[1][2].iter():
+            if i.attrib == {}:
                 continue
-            #print(i)
-            if i["from-id"] in self.concepts_by_id and i["to-id"] in self.concepts_by_id:
-                #print("Direct!")
-                self.graph.add_edge(i["from-id"], i["to-id"])
-            elif i["from-id"] in self.concepts_by_id: 
-                #print("from")
-                if i["to-id"] in self.phrases_by_id:
-                    self.graph.add_edge(i["from-id"], self.connections_by_from_id[i["to-id"]]['id'])
-                else:
-                    raise InvalidCmapFileException("", self._file_path)
-            elif i["to-id"] in self.concepts_by_id: 
-                #print("to")
-                if i["from-id"] in self.phrases_by_id:
-                    self.graph.add_edge(i["to-id"], self.connections_by_from_id[i["from-id"]]['id'])
-                else:
-                    raise InvalidCmapFileException("", self._file_path)
-            else:
-                print("Floating >:(")
-        """print(self.graph)
-        for i in self.concepts_by_id:
-            self.graph.nodes[i] = self.concepts_by_id[i]
-        for i in self.graph.nodes.items():
+            intital_connection_ids.append(i.attrib)
+        initial_connections_by_id = {x["id"]:x for x in intital_connection_ids} ### MARKED FOR DELETION ###
+        # FINDING CONNECTIONS 
+        connections_by_interaction: list[Connection] = []
+        self.connections = []
+
+        
+        """for i in list(self.concepts_by_id.keys()):
             print(i)
-        print(len(concept_list))"""
+            self.graph.add_node(i)
+
+        Cxl.show_map(self)"""
+
+        for i in self.phrases_by_id:
+            storage = {'from': [], 'to': []}
+            for j in initial_connections_by_id:
+                if initial_connections_by_id[j]["to-id"] == i:
+                    storage['from'].append(initial_connections_by_id[j]["from-id"])
+                if initial_connections_by_id[j]["from-id"] == i:
+                    storage['to'].append(initial_connections_by_id[j]["to-id"])
+                if initial_connections_by_id[j]["from-id"] == i and initial_connections_by_id[j]["to-id"] == i:
+                    print("MISSION CONTROL WE HAVE A PHRASE-PHRASE CONNECTION! ABORT ABORT ABOUR")
+                    exit(-1)
+                """if initial_connections_by_id[j]["from-id"] != i and initial_connections_by_id[j]["to-id"] != i: 
+                    # Missing Label
+                    self.connections.append(Connection(initial_connections_by_id[j]["from-id"], initial_connections_by_id[j]["to-id"], initial_connections_by_id[j]["id"]))
+                    print("Special! -> ",self.connections[-1])"""
+            bund = BundlePhrase(self.phrases_by_id[i]["label"], i, storage["from"], storage["to"]) 
+            self.connections.extend(bund.list_connections())
+
+        
+
+
+        """
+        
+        # self.connections_by_from_id = {x["from-id"]:x for x in connection_list}
+        # self.connections_by_to_id = {x["to-id"]:x for x in connection_list}
+
+        '''for i in self.concepts_by_id:
+            self.graph.add_node(i)'''
+        """
+
+        
+        
+
+
+        # self._fix_connections() ### MARKED FOR DELETION ###
+        d = {i:self.concepts_by_id[i]["label"] for i in self.concepts_by_id}
+        d.update({i:i for i in self.phrases_by_id})
+        for i in self.connections:
+            self.graph.add_edge(i.from_id, i.to_id, id=i.id, label = i.label)
+            if self.fuzzy:
+                self.graph[i.from_id][i.to_id].update({'weight':i.label})
         
 
 
 # Comp: # of nodes
 # Org: Density
+if __name__ == "__main__":
+    test = Cxl("concept_map_example.cxl")
+    test.parse_map()
+#   print(test.concepts_by_label)
+#   print({i:test.concepts_by_id[i]["label"] for i in test.concepts_by_id})
+#   print(test.graph.nodes.items())
+    Cxl.show_map(test)
--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-from cxlparser import cxl
+from cxlparser import Cxl
 import networkx as nx
 import defusedxml.ElementTree as ET
 import xml
 import matplotlib.pyplot as plt
 
 # https://cmap.ihmc.us/xml/CXL.html
-test = cxl("AeroCapture.cxl")
-test.parse_map()
-#print(test.concepts_by_label)
-#print({i:test.concepts_by_id[i]["label"] for i in test.concepts_by_id})
-#print(test.graph.nodes.items())
-cxl.show_map(test)
+
